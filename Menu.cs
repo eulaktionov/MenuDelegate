17cb2f7cc209652afefa55c9a4d5e2c86fadd472
eugeneLuck <eugeneLuck@gmail.com>
2022-11-12 13:09:18 +0300

    Добавьте файлы проекта.

---
+++ Menu.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using static System.Console;
+
+namespace StoreLib
+{
+    public class Menu
+    {
+        public class Command
+        {
+            public const int SeparatorValue = 99;
+            public string Text { get; }
+            public int Value { get; }
+            public Action<object> method;
+            public Func<object> getParam;
+            public Command(string text, int value, 
+                Action<object> method, Func<object> getParam) 
+                => (Text, Value, this.method, this.getParam) 
+                = (text, value, method, getParam);
+        }
+        const string menuSeparator = "================";
+        const string separator = "------";
+        const string prompt = "--->: ";
+        const string menuPrompt = "Choose command:";
+        const string valueError = "No such command!";
+        const string numberError = "Not a number!";
+        List<Command> commands;
+        
+        public Menu(Func<Menu.Command[]> initMenu)
+        {
+            commands = new List<Command>(initMenu?.Invoke());
+            AlignText();
+        }
+        void AlignText() => commands = commands.Select
+                (cmd => new Command
+                    (cmd.Text.PadRight(commands.Max(cmd => cmd.Text.Length)), 
+                    cmd.Value, cmd.method, cmd.getParam)
+                ).ToList<Command>();
+        public void Run() { while (GetCommand()) {} }
+        public bool GetCommand()
+        {
+            WriteLine(menuSeparator);
+            WriteLine(menuPrompt);
+            WriteLine(separator);
+            foreach (var it in commands)
+            {
+                if (it.Value == Command.SeparatorValue)
+                {
+                    WriteLine(separator);
+                }
+                else
+                {
+                    WriteLine($"{it.Text}{it.Value,3}");
+                }
+            }
+            int value = GetInt(prompt);
+            Command command;
+            while((command = FindCommand(value)) is null)
+            {
+                WriteLine(valueError);
+                value = GetInt(prompt);
+            }
+            if (command.method is null)
+            {
+                return false;
+            }
+            object param = command.getParam == null ? null 
+                : command.getParam();
+            command.method?.Invoke(param);
+            return true;
+        }
+        public Command? FindCommand(int value)
+            => commands.FirstOrDefault(cmd => cmd.Value == value);
+        
+        public static int GetInt(string prompt)
+        {
+            int value;
+            Write(prompt);
+            bool correct = int.TryParse(Console.ReadLine(), out value);
+            while(!correct)
+            {
+                WriteLine(numberError);
+                Write(prompt);
+                correct = int.TryParse(Console.ReadLine(), out value);
+            }
+            return value;
+        }
+        public static double GetDouble(string prompt)
+        {
+            double value;
+            Write(prompt);
+            bool correct = double.TryParse(Console.ReadLine(), out value);
+            while (!correct)
+            {
+                WriteLine(numberError);
+                Write(prompt);
+                correct = double.TryParse(Console.ReadLine(), out value);
+            }
+            return value;
+        }
+        public static decimal GetDecimal(string prompt)
+        {
+            decimal value;
+            Write(prompt);
+            bool correct = decimal.TryParse(Console.ReadLine(), out value);
+            while (!correct)
+            {
+                WriteLine(numberError);
+                Write(prompt);
+                correct = decimal.TryParse(Console.ReadLine(), out value);
+            }
+            return value;
+        }
+        public static string GetString(string prompt)
+        {
+            Write(prompt);
+            return Console.ReadLine();
+        }
+        public static string GetPassword(string prompt)
+        {
+            Write(prompt);
+            string password = string.Empty;
+            while(true)
+            {
+                ConsoleKeyInfo keyInfo = ReadKey(true); 
+                switch(keyInfo.Key)
+                {
+                    case ConsoleKey.Enter:
+                        WriteLine();
+                        return password;
+                    case ConsoleKey.Backspace:
+                        Write("\b \b");
+                        password = password.Substring(0, password.Length - 1);
+                        break;
+                    default:
+                        password += keyInfo.KeyChar;
+                        Write("*");
+                        break;
+                }
+            }
+        }
+    }
+
+    public static class AddExtension
+    {
+        const string numberError = "Not a number!";
+        public static string Read(this string prompt)
+        {
+            Write(prompt);
+            return Console.ReadLine();
+        }
+        public static int ReadInt(this string prompt)
+        {
+            int value;
+            Write(prompt);
+            bool correct = int.TryParse(Console.ReadLine(), out value);
+            while (!correct)
+            {
+                WriteLine(numberError);
+                Write(prompt);
+                correct = int.TryParse(Console.ReadLine(), out value);
+            }
+            return value;
+        }
+        public static double ReadDouble(this string prompt)
+        {
+            double value;
+            Write(prompt);
+            bool correct = double.TryParse(Console.ReadLine(), out value);
+            while (!correct)
+            {
+                WriteLine(numberError);
+                Write(prompt);
+                correct = double.TryParse(Console.ReadLine(), out value);
+            }
+            return value;
+        }
+        public static decimal ReadDecimal(this string prompt)
+        {
+            decimal value;
+            Write(prompt);
+            bool correct = decimal.TryParse(Console.ReadLine(), out value);
+            while (!correct)
+            {
+                WriteLine(numberError);
+                Write(prompt);
+                correct = decimal.TryParse(Console.ReadLine(), out value);
+            }
+            return value;
+        }
+        public static string ReadPassword(this string prompt)
+        {
+            Write(prompt);
+            string password = string.Empty;
+            while (true)
+            {
+                ConsoleKeyInfo keyInfo = ReadKey(true);
+                switch (keyInfo.Key)
+                {
+                    case ConsoleKey.Enter:
+                        WriteLine();
+                        return password;
+                    case ConsoleKey.Backspace:
+                        Write("\b \b");
+                        password = password.Substring(0, password.Length - 1);
+                        break;
+                    default:
+                        password += keyInfo.KeyChar;
+                        Write("*");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿using System.Reflection.Metadata;
+
+using StoreLib;
+using static System.Console;
+using static System.Console;
+
+WriteLine("Menu with Delegate");
+int x = 10;
+object GetParam2()
+{
+    return x;
+}
+object GetParam3()
+{
+    return Menu.GetString("Enter word: ");
+}
+object GetParam4()
+{
+    string name = Menu.GetString("Enter name: ");
+    int age = Menu.GetInt("Enter age: ");
+    return new Person(name, age);
+}
+
+Func<Menu.Command[]> initMenu = () => new Menu.Command[]
+{
+    new Menu.Command("CmdNull", (int)AppCommand.Command1, Method1, null),
+    new Menu.Command("CommandInt", (int)AppCommand.Command2, Method2, GetParam2),
+    new Menu.Command("CommandStr", (int)AppCommand.Command3, Method3, GetParam3),
+    new Menu.Command("CommandPerson", (int)AppCommand.Command4, Method4, GetParam4),
+    new Menu.Command("Exit", (int)AppCommand.Exit, null, null)
+};
+Menu menu = new Menu(initMenu);
+menu.Run();
+
+void Method1(object par1 = null)
+{
+    WriteLine("Method1");
+    string some = "Enter some: ".Read();
+    Console.WriteLine(some);
+}
+void Method2(object number)
+{
+    WriteLine("Method2");
+    Console.WriteLine((int)number);
+}
+void Method3(object word)
+{
+    WriteLine("Method3");
+    Console.WriteLine((string)word);
+}
+void Method4(object person)
+{
+    WriteLine("Method4");
+    Person p = person as Person;
+    Console.WriteLine($"{p.name} : {p.age}");
+}
+
+enum AppCommand 
+{ 
+    Command1 = 1, 
+    Command2,
+    Command3,
+    Command4,
+    Exit = 0
+};
+
+class Person
+{
+    public string name;
+    public int age;
+    public Person(string name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }   
+}
+
+
